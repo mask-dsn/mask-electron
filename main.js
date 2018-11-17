3fda2633088f187cda3f7e07cd253db32e87100b
Philip Zhan <h6zhan@gmail.com>
2018-11-17 15:08:03 -0500

    changed to js

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require("electron");
 
-let mainWindow: Electron.BrowserWindow;
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
 
 function createWindow() {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({
-		height: 600,
-		width: 800,
-	});
+	mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
 	// and load the index.html of the app.
-	mainWindow.loadFile(path.join(__dirname, "../index.html"));
+	mainWindow.loadFile("index.html");
 
 	// Open the DevTools.
-	// mainWindow.webContents.openDevTools();
+	//   mainWindow.webContents.openDevTools()
 
 	// Emitted when the window is closed.
-	mainWindow.on("closed", () => {
+	mainWindow.on("closed", function() {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
@@ -31,7 +30,7 @@ function createWindow() {
 app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on("window-all-closed", function() {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== "darwin") {
@@ -39,13 +38,13 @@ app.on("window-all-closed", () => {
 	}
 });
 
-app.on("activate", () => {
-	// On OS X it"s common to re-create a window in the app when the
+app.on("activate", function() {
+	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) {
 		createWindow();
 	}
 });
 
-// In this file you can include the rest of your app"s specific main process
+// In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
