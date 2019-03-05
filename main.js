ad165bfb77146ab8b23e46e95d78fc88ef334f61
Philip Zhan <h6zhan@gmail.com>
2019-03-05 15:28:20 -0500

    updated electron

--- main.js
+++ main.js
@@ -13,8 +13,15 @@ let mainWindow;
 
 function createWindow() {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({ width: 800, height: 1000, x: 0, y: 0 });
-
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 1100,
+		x: 0,
+		y: 0,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
 	// and load the index.html of the app.
 	mainWindow.loadFile("index.html");
 
@@ -22,7 +29,7 @@ function createWindow() {
 	mainWindow.webContents.openDevTools({ mode: "bottom" });
 
 	// Emitted when the window is closed.
-	mainWindow.on("closed", function() {
+	mainWindow.on("closed", function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
@@ -60,7 +67,7 @@ function createWindow() {
 app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", function () {
 	// On OS X it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== "darwin") {
@@ -68,7 +75,7 @@ app.on("window-all-closed", function() {
 	}
 });
 
-app.on("activate", function() {
+app.on("activate", function () {
 	// On OS X it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) {
