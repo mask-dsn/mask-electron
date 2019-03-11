c49c02666b28f09ab65233cd1e602bff225eb524
Philip Zhan <h6zhan@gmail.com>
2019-03-10 22:59:19 -0400

    write to tracker works

--- app/utils/tracker.js
+++ app/utils/tracker.js
@@ -1,11 +1,32 @@
-const ngrok = require('ngrok');
+import firebase from 'firebase';
+import ngrok from 'ngrok';
 
 const http_port = process.env.HTTP_PORT || 3001;
 const p2p_port = process.env.P2P_PORT || 6001;
+const firebaseConfig = {
+  apiKey: 'AIzaSyAY5bhwqUUDd0bybwqGDeb0LPZTRSdC3kY',
+  authDomain: 'dsn-tracker.firebaseapp.com',
+  databaseURL: 'https://dsn-tracker.firebaseio.com',
+  projectId: 'dsn-tracker',
+  storageBucket: 'dsn-tracker.appspot.com',
+  messagingSenderId: '1043546948603'
+};
+const collectionName = 'tracker';
+const documentName = 'peer';
 
 async function connect() {
-  const url = await ngrok.connect(http_port);
+  let url = await ngrok.connect(p2p_port);
+  url = url.replace(/^https?:\/\//, '');
   console.log(`ngrok URL: ${url}`);
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
+  const docRef = db.collection(collectionName).doc(documentName);
+  const newPeers = docRef.update({
+    url: firebase.firestore.FieldValue.arrayUnion(url)
+  });
 }
 
 async function disconnect() {
